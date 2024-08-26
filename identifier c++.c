b7a63d37f7c07b8c55e5346019ff21c4f61b60aa
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:01:35 +0530

    Create identifier c++.c

---
+++ identifier c++.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // good variable name
+    int minutePerHour = 60;
+    // okk but not  so  easy to understand
+    int m = 60;
+    cout << minutePerHour<<"\n";
+    cout << m;
+    return 0;
+}
